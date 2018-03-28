23948ff806de881dcaa803aa4e075b987e683a6a
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:39:04 +0430

    Add Deposit Model Input/Output Validators

--- common/models/deposit.js
+++ common/models/deposit.js
@@ -1,3 +1,35 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+var startTypeConfig = require('../../config/StartType.json')
+var depositTypeConfig = require('../../config/DepositType.json')
+var capacityTypeConfig = require('../../config/CapacityType.json')
+var guaranteeTypeConfig = require('../../config/GuaranteeType.json')
+
 module.exports = function(deposit) {
 
+  var statusList = []
+  for (var key in modelStatusConfig) 
+		statusList.push(modelStatusConfig[key])
+
+  var startTypeList = []
+  for (var key in startTypeConfig) 
+		startTypeList.push(startTypeConfig[key])
+
+  var depositTypeList = []
+  for (var key in depositTypeConfig) 
+		depositTypeList.push(depositTypeConfig[key])
+
+  var capacityTypeList = []
+  for (var key in capacityTypeConfig) 
+		capacityTypeList.push(capacityTypeConfig[key])
+
+  var guaranteeTypeList = []
+  for (var key in guaranteeTypeConfig) 
+		guaranteeTypeList.push(guaranteeTypeConfig[key])
+
+	deposit.validatesInclusionOf('status', {in: statusList})
+	deposit.validatesInclusionOf('startType', {in: startTypeList})
+	deposit.validatesInclusionOf('depositType', {in: depositTypeList})
+	deposit.validatesInclusionOf('capacityType', {in: capacityTypeList})
+	deposit.validatesInclusionOf('guaranteeType', {in: guaranteeTypeList})
+	
 }
