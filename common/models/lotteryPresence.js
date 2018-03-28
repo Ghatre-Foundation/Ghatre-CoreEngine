4c09df1fdfd2ff9f7aa7643ce0ec9d0804430c70
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:40:14 +0430

    Add LotteryPresence Model Input/Output Validators

--- common/models/lotteryPresence.js
+++ common/models/lotteryPresence.js
@@ -1,3 +1,11 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+
 module.exports = function(lotteryPresence) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+    statusList.push(modelStatusConfig[key])
+
+	lotteryPresence.validatesInclusionOf('status', {in: statusList})
 
 }
