5933c6e0a3d87a003802917e2743d1ee3ec2d998
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:39:56 +0430

    Add LotteryFinance Model Input/Output Validators

--- common/models/lotteryFinance.js
+++ common/models/lotteryFinance.js
@@ -1,3 +1,17 @@
-module.exports = function(Lotteryfinance) {
+var operationStatusConfig = require('../../config/OperationStatus.json')
+var transactionTypeConfig = require('../../config/TransactionType.json')
+
+module.exports = function(lotteryFinance) {
+
+  var statusList = []
+  for (var key in operationStatusConfig) 
+    statusList.push(operationStatusConfig[key])
+
+  var typeList = []
+  for (var key in transactionTypeConfig) 
+		typeList.push(transactionTypeConfig[key])
+
+	lotteryFinance.validatesInclusionOf('status', {in: statusList})
+	lotteryFinance.validatesInclusionOf('type', {in: typeList})
 
 }
