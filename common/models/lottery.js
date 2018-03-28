10d7359c0807e4a6f76a7b32eab742e35a5d92d1
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:39:46 +0430

    Add Lottery Model Input/Output Validators

--- common/models/lottery.js
+++ common/models/lottery.js
@@ -1,3 +1,35 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+var startTypeConfig = require('../../config/StartType.json')
+var drawTypeConfig = require('../../config/DrawType.json')
+var capacityTypeConfig = require('../../config/CapacityType.json')
+var guaranteeTypeConfig = require('../../config/GuaranteeType.json')
+
 module.exports = function(lottery) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+		statusList.push(modelStatusConfig[key])
+
+  var startTypeList = []
+  for (var key in startTypeConfig) 
+		startTypeList.push(startTypeConfig[key])
+
+  var drawTypeList = []
+  for (var key in drawTypeConfig) 
+		drawTypeList.push(drawTypeConfig[key])
+
+  var capacityTypeList = []
+  for (var key in capacityTypeConfig) 
+		capacityTypeList.push(capacityTypeConfig[key])
+
+  var guaranteeTypeList = []
+  for (var key in guaranteeTypeConfig) 
+		guaranteeTypeList.push(guaranteeTypeConfig[key])
+
+	lottery.validatesInclusionOf('status', {in: statusList})
+	lottery.validatesInclusionOf('startType', {in: startTypeList})
+	lottery.validatesInclusionOf('drawType', {in: drawTypeList})
+	lottery.validatesInclusionOf('capacityType', {in: capacityTypeList})
+	lottery.validatesInclusionOf('guaranteeType', {in: guaranteeTypeList})
 
 }
