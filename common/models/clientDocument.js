7ed563653bfde998f8334b36af527f9e24575548
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:38:28 +0430

    Add ClientDocument Model Input/Output Validators

--- common/models/clientDocument.js
+++ common/models/clientDocument.js
@@ -1,3 +1,12 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+
 module.exports = function(clientDocument) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+    statusList.push(modelStatusConfig[key])
+
+	clientDocument.validatesInclusionOf('nationalCardStatus', {in: statusList})
+	clientDocument.validatesInclusionOf('birthCertificateStatus', {in: statusList})
 
 }
