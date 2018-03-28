ee56026ea7ed1d5e1850725636dab36abd44c03e
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:40:05 +0430

    Add LotteryInstance Model Input/Output Validators

--- common/models/lotteryInstance.js
+++ common/models/lotteryInstance.js
@@ -1,3 +1,11 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+
 module.exports = function(lotteryInstance) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+    statusList.push(modelStatusConfig[key])
+
+	lotteryInstance.validatesInclusionOf('status', {in: statusList})
 
 }
