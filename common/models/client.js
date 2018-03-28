a7a4ca672919b63e5341ac68c659dedc466f69a3
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:38:19 +0430

    Add Client Model Input/Output Validators

--- common/models/client.js
+++ common/models/client.js
@@ -1,3 +1,17 @@
+var availabilityStatusConfig = require('../../config/AvailabilityStatus.json')
+var genderConfig = require('../../config/ClientGender.json')
+
 module.exports = function(client) {
+
+  var availabilityList = []
+  for (var key in availabilityStatusConfig) 
+		availabilityList.push(availabilityStatusConfig[key])
+
+  var genderList = []
+  for (var key in genderConfig) 
+		genderList.push(genderConfig[key])
+
+	client.validatesInclusionOf('status', {in: availabilityList})
+	client.validatesInclusionOf('gender', {in: genderList})
 
 }
