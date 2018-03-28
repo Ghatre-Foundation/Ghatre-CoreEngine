7152a804f04e5b25ef6c6b265d4c391f483d62aa
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:38:58 +0430

    Add ClientReceipt Model Input/Output Validators

--- common/models/clientReceipt.js
+++ common/models/clientReceipt.js
@@ -1,3 +1,17 @@
+var operationStatusConfig = require('../../config/OperationStatus.json')
+var transactionTypeConfig = require('../../config/TransactionType.json')
+
 module.exports = function(clientReceipt) {
+
+  var statusList = []
+  for (var key in operationStatusConfig) 
+    statusList.push(operationStatusConfig[key])
+
+  var typeList = []
+  for (var key in transactionTypeConfig) 
+		typeList.push(transactionTypeConfig[key])
+
+	clientReceipt.validatesInclusionOf('status', {in: statusList})
+	clientReceipt.validatesInclusionOf('type', {in: typeList})
 
 }
