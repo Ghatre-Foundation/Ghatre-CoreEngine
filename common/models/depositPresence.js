29fe8524c083a7c249d5702adab6be7848f3d1c3
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:39:29 +0430

    Add DepositPresence Model Input/Output Validators

--- common/models/depositPresence.js
+++ common/models/depositPresence.js
@@ -1,3 +1,11 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+
 module.exports = function(depositPresence) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+    statusList.push(modelStatusConfig[key])
+
+	depositPresence.validatesInclusionOf('status', {in: statusList})
 
 }
