bce980c722403b1740c4eb6e2c142087b95edca7
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:40:23 +0430

    Add Statute Model Input/Output Validators

--- common/models/statute.js
+++ common/models/statute.js
@@ -1,3 +1,17 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+var visibilityTypeConfig = require('../../config/VisibilityType.json')
+
 module.exports = function(statute) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+    statusList.push(modelStatusConfig[key])
+
+  var visibilityTypeList = []
+  for (var key in visibilityTypeConfig) 
+		visibilityTypeList.push(visibilityTypeConfig[key])
+
+	statute.validatesInclusionOf('status', {in: statusList})
+	statute.validatesInclusionOf('type', {in: visibilityTypeList})
 
 }
