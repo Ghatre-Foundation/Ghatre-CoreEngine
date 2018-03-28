d08f56b76b3bab4e472ba9c0f1f36526545da93f
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:39:12 +0430

    Add DepositFinance Model Input/Output Validators

--- common/models/depositFinance.js
+++ common/models/depositFinance.js
@@ -1,3 +1,17 @@
-module.exports = function(depositfinance) {
+var operationStatusConfig = require('../../config/OperationStatus.json')
+var transactionTypeConfig = require('../../config/TransactionType.json')
+
+module.exports = function(depositFinance) {
+
+  var statusList = []
+  for (var key in operationStatusConfig) 
+    statusList.push(operationStatusConfig[key])
+
+  var typeList = []
+  for (var key in transactionTypeConfig) 
+		typeList.push(transactionTypeConfig[key])
+
+	depositFinance.validatesInclusionOf('status', {in: statusList})
+	depositFinance.validatesInclusionOf('type', {in: typeList})
 
 }
