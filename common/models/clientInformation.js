b2b652b75b1ec4446b0287bf413d06c0c62fce5c
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:38:38 +0430

    Add ClientInformation Model Input/Output Validators

--- common/models/clientInformation.js
+++ common/models/clientInformation.js
@@ -1,3 +1,29 @@
-module.exports = function(clientInfo) {
+var countryListConfig = require('../../config/listOfCountries.json')
+var provinceListConfig = require('../../config/listOfProvinces.json')
+var cityListConfig = require('../../config/listOfCities.json')
+
+module.exports = function(clientInformation) {
+
+  var countryList = []
+  for (var i = 0; i < countryListConfig.length; i++) {
+		var model = countryListConfig[i]
+		countryList.push(model.name)
+	}
+
+  var provinceList = []
+  for (var i = 0; i < provinceListConfig.length; i++) {
+		var model = provinceListConfig[i]
+		provinceList.push(model.name)
+	}
+
+  var cityList = []
+  for (var i = 0; i < cityListConfig.length; i++) {
+		var model = cityListConfig[i]
+		cityList.push(model.name)
+	}
+
+	clientInformation.validatesInclusionOf('country', {in: countryList})
+	clientInformation.validatesInclusionOf('province', {in: provinceList})
+	clientInformation.validatesInclusionOf('city', {in: cityList})
 
 }
