89c354daecff9161f94c24fb3431cdd1fb886873
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:38:12 +0430

    Add Agreement Model Input/Output Validators

--- common/models/agreement.js
+++ common/models/agreement.js
@@ -1,3 +1,17 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+var visibilityTypeConfig = require('../../config/VisibilityType.json')
+
 module.exports = function(agreement) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+    statusList.push(modelStatusConfig[key])
+
+  var visibilityTypeList = []
+  for (var key in visibilityTypeConfig) 
+		visibilityTypeList.push(visibilityTypeConfig[key])
+
+	agreement.validatesInclusionOf('status', {in: statusList})
+	agreement.validatesInclusionOf('type', {in: visibilityTypeList})
 
 }
