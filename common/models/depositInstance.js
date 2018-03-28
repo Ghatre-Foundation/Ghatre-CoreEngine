48124aee9032b6a41b0c57ee0bf67befbf3e9f81
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-03-28 23:39:20 +0430

    Add DepositInstance Model Input/Output Validators

--- common/models/depositInstance.js
+++ common/models/depositInstance.js
@@ -1,3 +1,11 @@
+var modelStatusConfig = require('../../config/ModelStatus.json')
+
 module.exports = function(depositInstance) {
+
+  var statusList = []
+  for (var key in modelStatusConfig) 
+    statusList.push(modelStatusConfig[key])
+
+	depositInstance.validatesInclusionOf('status', {in: statusList})
 
 }
