3ea8c461a7b1fa9c8abb52051aceb8f73a92d62d
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-04-08 10:41:47 +0430

    Load Agreement HTML Templates and Add to App

---
+++ server/boot/z-agreementTemplateLoader.js
@@ -0,0 +1,31 @@
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function (app) {
+	var directory = path.resolve(__dirname + '/../../template/agreement/')
+	if (!app.templates)
+		app.templates = {}
+	app.templates.agreements = {}
+  fs.readdir(directory, function (err, files) {
+    if (err)
+      console.error(err)
+    files.forEach(function (file) {
+			var address = directory + '/' + file
+			if (path.extname(directory + '/' + file) === '.js')
+				return
+      fs.open(address, 'r+', function (err, fd) {
+        if (err)
+          console.error(err)
+        fs.readFile(fd, function (err, data) {
+          if (err)
+            console.error(err)
+					app.templates.agreements[file.slice(0, -5)] = data.toString()
+          fs.close(fd, function (err) {
+            if (err)
+              console.error(err)
+          })
+        })
+      })
+    })
+  })
+}
