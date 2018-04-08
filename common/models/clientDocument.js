d6d28d8a2fbfd74009b907327fc7dd1e4bb0f3f0
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-04-08 10:34:23 +0430

    Implement Logic Codes Related to Client Document Model and Adding ACL

--- common/models/clientDocument.js
+++ common/models/clientDocument.js
@@ -1,5 +1,9 @@
 var modelStatusConfig = require('../../config/ModelStatus.json')
 
+var app = require('../../server/server')
+var utility = require('../../public/utility')
+var roleManager = require('../../public/roleManager')
+
 module.exports = function(clientDocument) {
 
   var statusList = []
@@ -8,5 +12,40 @@ module.exports = function(clientDocument) {
 
 	clientDocument.validatesInclusionOf('nationalCardStatus', {in: statusList})
 	clientDocument.validatesInclusionOf('birthCertificateStatus', {in: statusList})
+
+	clientDocument.beforeRemote('updateById', function (ctx, modelInstance, next) {
+    roleManager.getRolesById(app, ctx.args.options.accessToken.userId.toString(), function (err, response) {
+      if (err)
+        return next(err)
+      if (response.roles.length != 0) {
+        var whiteList = ['nationalCardStatus', 'nationalCardAdminMessage', 'birthCertificateStatus', 'birthCertificateAdminMessage']
+        if (!utility.whiteChecker(ctx.args.data, whiteList))
+          return next(new Error('White Parameters'))
+      }
+      else
+        return next(new Error('Only admin and founder are allowed'))
+    })
+	})
+
+	clientDocument.afterRemote('updateById', function (ctx, modelInstance, next) {
+    if (modelInstance.nationalCardStatus === modelStatusConfig.approved && modelInstance.birthCertificateStatus === modelStatusConfig.approved) {
+      var client = app.models.client
+      client.findById(modelInstance.clientId.toString(), function(err, clientModel) {
+        if (err)
+          return next(err)
+        if (!clientModel)
+          return next(new Error('Model not Found'))
+        var clientNewData = {
+          'stage': Number(clientModel.stage) + 1,
+          'status': availabilityStatusConfig.available
+        }
+        clientModel.updateAttributes(clientNewData, function(err, clientUpdatedModel) {
+          if (err)
+            return next(err)
+          return next()
+        })
+      })      
+    }        
+  })
 
 }
